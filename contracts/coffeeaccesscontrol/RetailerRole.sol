2f90f64f88e8ea4cd35cac4c916d9ce012d333b6
sameerdewan <sameer.dewan@itential.com>
2020-06-14 16:46:31 -0400

    completed access level control roles contracts

---
+++ contracts/coffeeaccesscontrol/RetailerRole.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+import './Roles.sol';
+
+
+contract RetailerRole {
+    using Roles for Roles.Role;
+
+    event RetailerAdded(address indexed account);
+    event RetailedRemoved(address indexed account);
+
+    Roles.Role private retailers;
+
+    constructor() public {
+        _addRetailer(msg.sender);
+    }
+
+    modifier onlyRetailer() {
+        require(isRetailer(msg.sender), 'Error');
+        _;
+    }
+
+    function isRetailer(address account) public view returns (bool) {
+        return retailers.has(account);
+    }
+
+    function addRetailer(address account) public onlyRetailer {
+        _addRetailer(account);
+    }
+
+    function renounceRetailer() public {
+        _removeRetailer(msg.sender);
+    }
+
+    function _addRetailer(address account) internal {
+        retailers.add(account);
+        emit RetailerAdded(account);
+    }
+
+    function _removeRetailer(address account) internal {
+        retailers.remove(account);
+        emit RetailedRemoved(account);
+    }
+}
--- contracts/coffeeaccesscontrol/Roles.sol
+++ contracts/coffeeaccesscontrol/Roles.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.0;
 
+
 library Roles {
   struct Role {
     mapping (address => bool) bearer;
