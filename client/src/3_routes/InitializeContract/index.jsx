46858b7c5923f7895cfa31cec1c39107cb5ee97b
sameerdewan <sameer.dewan@itential.com>
2020-06-16 01:59:22 -0400

    client css

--- client/src/3_routes/InitializeContract/index.jsx
+++ client/src/3_routes/InitializeContract/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import {Container, Row, Col, FormControl} from 'react-bootstrap';
+import {Container, Row, Col, FormControl, Alert} from 'react-bootstrap';
 import {withContext} from '../../1_context';
 import './index.css';
 
@@ -14,7 +14,7 @@ function InitializeContract(props) {
                     <section className={'abi-label'}>
                         <center>
                             <code>
-                                Supply Chain ABI Code
+                                Base Supply Chain ABI Code
                             </code>
                         </center>
                     </section>
@@ -28,12 +28,32 @@ function InitializeContract(props) {
                         <section className={'initialize-contract-label'}>
                             <center>
                                 <code>
-                                    Deploy an Instance of the Coffee Supply Chain Contract
+                                    Deploy a Supply Chain Contract
                                 </code>
                             </center>
                         </section>
                         <section className={'initialize-contract-container'}>
-                            <br/><br/>
+                            <br/>
+                            <Alert variant={'primary'}>
+                                <Alert.Heading>Deploying a Contract</Alert.Heading>
+                                <p>
+                                    To deploy a supply chain contract, <u>you will need to access this page
+                                    from the address intended to be the owner of this particular instance
+                                    of the supply chain contract</u>. The owner cannot be a farmer, distributor,
+                                    or retailer. 
+                                    <br/><br/>
+                                    The owner of this contract will be able to add farmers, distributors, and retailers
+                                    to the deployed smart contract instance, whom in turn may participate in the supply chain.
+                                </p>
+                                <hr />
+                                <p className="mb-0">
+                                    <i>
+                                        You must have access to the addresses provided for the farmer, distributor, and retailer
+                                        to interact with the contract's role based actions.
+                                    </i>
+                                </p>
+                            </Alert>
+                            <br/>
                             <FormControl placeholder={'Enter originFarmerID (initialFarmer)'}/>
                             <br/>
                             <FormControl placeholder={'Enter distributorID (initialDistributor)'}/>
